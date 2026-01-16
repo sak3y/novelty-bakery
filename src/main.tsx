ac819b451dd73b1acefe2ff9b59d93ed981ad83f
sakey01 <sheikh36ahmed@gmail.com>
2026-01-16 17:59:50 +0000

    file name changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./hooks/scrollTop.tsx";
+import ScrollToTop from "./hooks/useScroll.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/novelty-bakery">
